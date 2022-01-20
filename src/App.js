487305105eac6197ea27aac20a3e1dc8c5b8f0e7
Fernanda <fernanda_depaula@poli.ufrj.br>
2022-01-20 09:45:27 -0300

    Adiciona funcionalidade de adicionar objetos

--- src/App.js
+++ src/App.js
@@ -1,65 +1,9 @@
-import './App.css';
+import React from 'react';
 import "./style.css";
 import logo from './assets/logo.jpeg';
-import React, {useState, useRef} from "react";
 import ListaCarros from './Components/ListaCarros/ListaCarros.js';
 
-function App() {
-  const [marca, setMarca] = useState();
-  const [modelo, setModelo] = useState();
-  const [cor, setCor] = useState();
-  const [ano, setAno] = useState();
-  const [motor, setMotor] = useState();
-  const [estoque, setEstoque] = useState();
-  const [preço, setPreço] = useState();
-
-  const handleAddCarro=()=>{
-    const newItem={id:1, marca:marca, modelo:modelo, cor:cor, ano:ano, motor:motor, estoque:estoque, preço:preço}
-    const newList=list.concat(newItem)
-    setList(newList) 
-  }
-
-  const oldList=[
-    {marca: "CHEVROLET",
-    modelo: "CORVETTE",
-    cor: "VERMELHO",
-    ano: 2021,
-    motor: "V8 LT6 5-5I",
-    estoque: 2,
-    preço: "R$ 9.999,99"
-  }
-  ];
-
-  const[list, setList]=useState(oldList)
-
-  const renderList = list.map((item,index)=>(
-    <li key={index}>
-      <div className='card-container'>
-        <h3>
-          <strong className='card-title'>Marca</strong>: {item.marca}
-        </h3>
-        <h3>
-          <strong className='card-title'>Modelo</strong>: {item.modelo}
-        </h3>
-        <h3>
-          <strong className='card-title'>Cor</strong>: {item.cor}
-        </h3>
-        <h3>
-          <strong className='card-title'>Ano</strong>: {item.ano}
-        </h3>
-        <h3>
-          <strong className='card-title'>Motor</strong>: {item.motor}
-        </h3>
-        <h3>
-          <strong className='card-title'>Estoque</strong>: {item.estoque}
-        </h3>
-        <h3>
-          <strong className='card-title'>Preço</strong>: {item.preço}
-        </h3>
-      </div>
-      
-    </li>
-    ));
+export default function App() {
 
   return (
     <div id="Wrapper">
@@ -81,22 +25,13 @@ function App() {
           <input class="inputText" type="text" placeholder='Buscar por modelo ou marca'/>         
         </div> 
             
-      </div>    
+      </div>   
 
-      <div class="main">
-      
-        <input class="inputVeiculos" type="text" placeholder='Marca' onChange={(e)=>setMarca(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Modelo' onChange={(e)=>setModelo(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Cor' onChange={(e)=>setCor(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Ano de Fabricaçao' onChange={(e)=>setAno(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Motor' onChange={(e)=>setMotor(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Estoque' onChange={(e)=>setEstoque(e.currentTarget.value)}/>
-        <input class="inputVeiculos" type="text" placeholder='Preço' onChange={(e)=>setPreço(e.currentTarget.value)}/>
+      <div class="lista">
+        <ListaCarros/>
+      </div> 
 
-        <button onClick={handleAddCarro}> Adicionar Veículo</button>
-
-        
-      </div>  
+  
 
       <div class="footer">
         <div class="bloco sobre">
@@ -116,4 +51,4 @@ function App() {
   );
 }
 
-export default App;
+
--- src/Components/Carro/Carro.js
+++ src/Components/Carro/Carro.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react';
+import './style.css'
 
-export default function Carro({carro}){
+export default function Card({data}){
     return (
-        <div>
-            <p>{carro.textoCarro}</p>
-        </div>
-    )
+        <div className='card-container'>
+            <h3>
+            <strong className='card-title'>Marca</strong>: {data.marca}
+            </h3>
+            <h3>
+            <strong className='card-title'>Modelo</strong>: {data.modelo}
+            </h3>
+            <h3>
+            <strong className='card-title'>Cor</strong>: {data.cor}
+            </h3>
+            <h3>
+            <strong className='card-title'>Ano</strong>: {data.ano}
+            </h3>
+            <h3>
+            <strong className='card-title'>Motor</strong>: {data.motor}
+            </h3>
+            <h3>
+            <strong className='card-title'>Estoque</strong>: {data.estoque}
+            </h3>
+            <h3>
+            <strong className='card-title'>Preço</strong>: {data.preço}
+            </h3>
+      </div>
+        );
 }
